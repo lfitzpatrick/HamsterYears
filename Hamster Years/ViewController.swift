244e7fa7f7ce4dbe2174d180f941acf0d44c937e
lfitzpatrick <lenard@iit.edu>
2017-07-24 01:23:54 -0500

    Initial

--- Hamster Years/ViewController.swift
+++ Hamster Years/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        // Test that everything works, remove once successful
+        print(ageTextField.text as Any)
+        
+        // Convert ageTextField.text to an integer using Int ()
+        // Since the value is optional use ! at the end of value
+        // Assign the value to ageInHamsterYears
+        // Use let instead of var
+        let ageInHamsterYears = Int (ageTextField.text!)! * 26
+        
+        // Convert value to String using String ()
+        // Assign the string to ageLabel.text
+        ageLabel.text = String (ageInHamsterYears)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
